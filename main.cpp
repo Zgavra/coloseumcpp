33917ef2824e460febdf811bd92155217a07d395
vojislav.gavrilov@gmail.com <vojislav.gavrilov@gmail.com>
2015-05-15 19:59:41 +0200

    adding the project
    project added with just the basic output

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << " Welcome to coloseum!" << endl;
+    cout <<" Please select one of the following options" << endl;
+    cout <<" 1. Add Gladiator" << endl;
+    cout <<" 2. Remove Gladiator" << endl;
+    cout <<" 3. Make a fight" << endl;
+    cout <<" 4. Exit" << endl;
+    return 0;
+}
